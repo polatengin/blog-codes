3e1ea3b13ffb2c9c3dd2a35ae6f20bd54495be00
Engin Polat <engin@enginpolat.com>
2017-12-21 15:31:15 +0300

    Windows 10 UWP Restart Project projesi eklendi

---
+++ windows-10-uwp-restart-project/RestartProject/App.xaml.cs
@@ -0,0 +1,59 @@
+﻿using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace RestartProject
+{
+  sealed partial class App : Application
+  {
+    public App()
+    {
+      this.InitializeComponent();
+    }
+
+    protected override void OnLaunched(LaunchActivatedEventArgs e)
+    {
+      Frame rootFrame = Window.Current.Content as Frame;
+
+      if (rootFrame == null)
+      {
+        rootFrame = new Frame();
+
+        if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+        {
+        }
+
+        Window.Current.Content = rootFrame;
+      }
+
+      if (e.PrelaunchActivated == false)
+      {
+        if (rootFrame.Content == null)
+        {
+          rootFrame.Navigate(typeof(MainPage), e.Arguments);
+        }
+        Window.Current.Activate();
+      }
+    }
+
+    protected override void OnActivated(IActivatedEventArgs args)
+    {
+      if (args.Kind == ActivationKind.Launch)
+      {
+        var cityName = (args as LaunchActivatedEventArgs).Arguments;
+
+        var rootFrame = Window.Current.Content as Frame;
+
+        if (rootFrame == null)
+        {
+          rootFrame = new Frame();
+          Window.Current.Content = rootFrame;
+        }
+
+        rootFrame.Navigate(typeof(MainPage), cityName);
+
+        Window.Current.Activate();
+      }
+    }
+  }
+}
---
+++ windows-10-uwp-restart-project/RestartProject/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Windows.ApplicationModel.Core;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+namespace RestartProject
+{
+  public sealed partial class MainPage : Page
+  {
+    public MainPage()
+    {
+      this.InitializeComponent();
+
+      this.lvCities.ItemsSource = new ObservableCollection<KeyValuePair<string, string>>()
+      {
+        new KeyValuePair<string, string>("İstanbul", "/Assets/istanbul.jpg"),
+        new KeyValuePair<string, string>("İzmir", "/Assets/izmir.jpg"),
+        new KeyValuePair<string, string>("Ankara", "/Assets/ankara.jpg"),
+        new KeyValuePair<string, string>("Bursa", "/Assets/bursa.jpg")
+      };
+    }
+
+    protected override void OnNavigatedTo(NavigationEventArgs e)
+    {
+      var cityName = e.Parameter as string;
+
+      if (!string.IsNullOrEmpty(cityName))
+      {
+        this.selected = (this.lvCities.ItemsSource as ObservableCollection<KeyValuePair<string, string>>).FirstOrDefault(c => c.Key == cityName);
+
+        this.lvCities.SelectedItem = this.selected;
+        this.imgSelected.Source = new BitmapImage(new Uri("ms-appx://" + this.selected.Value));
+      }
+    }
+
+    private KeyValuePair<string, string> selected;
+
+    private void lvCities_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+      this.selected = (KeyValuePair<string, string>)e.AddedItems[0];
+      this.imgSelected.Source = new BitmapImage(new Uri("ms-appx://" + this.selected.Value));
+    }
+
+    private async void btnRestart_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+    {
+      await CoreApplication.RequestRestartAsync(selected.Key ?? string.Empty);
+    }
+  }
+}
